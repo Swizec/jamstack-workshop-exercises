cbc388ddce165a8f98040dda7ed7617643990ec7
Swizec Teller <swizec@swizec.com>
2020-11-05 08:05:16 -0800

    Add theme cycling solution

--- exercise-1/gatsby/src/gatsby-plugin-theme-ui/index.js
+++ exercise-1/gatsby/src/gatsby-plugin-theme-ui/index.js
@@ -1,4 +1,33 @@
 import sketchy from "theme-ui-sketchy-preset"
 export default {
   ...sketchy,
+  colors: {
+    ...sketchy.colors,
+    modes: {
+      light: {
+        ...sketchy.colors,
+      },
+      dark: {
+        text: "#fff",
+        background: "#000",
+        copyBackground: "hsl(167, 6%, 42%)",
+        primary: "#0ff",
+        secondary: "#b0f",
+        accent: "#f0b",
+        muted: "#111",
+        gray: "#999",
+        lightgray: "#444",
+      },
+      deep: {
+        text: "hsl(210, 50%, 96%)",
+        background: "hsl(230, 25%, 18%)",
+        primary: "hsl(260, 100%, 80%)",
+        secondary: "hsl(290, 100%, 80%)",
+        highlight: "hsl(260, 20%, 40%)",
+        purple: "hsl(290, 100%, 80%)",
+        muted: "hsla(230, 20%, 0%, 20%)",
+        gray: "hsl(210, 50%, 60%)",
+      },
+    },
+  },
 }
--- exercise-1/gatsby/src/pages/index.js
+++ exercise-1/gatsby/src/pages/index.js
@@ -5,18 +5,31 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+import { Button, useColorMode } from "theme-ui"
+const modes = ["light", "dark", "deep"]
+
+const IndexPage = () => {
+  const [mode, setMode] = useColorMode()
+
+  function cycleMode() {
+    const i = (modes.indexOf(mode) + 1) % modes.length
+    setMode(modes[i])
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Button onClick={cycleMode}>Cycle mode</Button>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
