993d244bf9307ba3e0a0f30b663ff08402ecd6e9
Swizec Teller <swizec@swizec.com>
2020-11-08 20:49:21 -0800

    NextJS MDX solution

---
+++ exercise-3/nextjs/components/Layout.js
@@ -0,0 +1,13 @@
+import { Container } from "theme-ui";
+import Head from "next/head";
+
+export const Layout = ({ title, children }) => {
+    return (
+        <Container>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            {children}
+        </Container>
+    );
+};
